14b0c8550e02a3d8aa77b70624653fab7a7f5577
michaelsatumba <mjsatumba@gmail.com>
2021-12-16 14:36:09 -0800

    Initializing

--- components/Initialzing.js
+++ components/Initialzing.js
@@ -1,32 +1,19 @@
-import Image from "next/image";
 import { useMoralis } from "react-moralis";
 import profilePic from '../public/images/pic.png'
-import Initialzing from "./Initialzing";
+import Image from "next/image";
 
-function Login() {
-    const { authenticate, isAuthenticating } = useMoralis();
 
-    if (isAuthenticating) {
-           return(
-            <Initialzing/> 
-           ) 
-        }
-
+function Initialzing() {
     return (
         <div className="bg-black relative">
             <h1>I am the Login Screen</h1>
             <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
                
-                <Image
-                    className="object-cover rounded-full" 
-                    src={profilePic}
-                    height={200}
-                    width={200}
-                />
+                
                
                 <button 
-                    onClick={authenticate}
-                    className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Login to the Metaverse
+                    
+                    className="bg-red-500 rounded-lg p-5 font-bold animate-pulse">Initializing...
                 </button>
             </div>
 
@@ -41,4 +28,4 @@ function Login() {
     )
 }
 
-export default Login
+export default Initialzing
